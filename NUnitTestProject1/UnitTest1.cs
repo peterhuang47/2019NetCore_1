690e97d77680d75859673586b81554f2b6b1d3a9
Peter <g5447@hotmail.com>
2020-05-08 17:37:16 +0800

    Test debug

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -14,11 +14,10 @@ namespace NUnitTestProject1
 
         [Test]
         public void Test1()
-        {
-            throw new System.Exception("NUnitTestProject1.Tests.Test1");
-            ILogger<IndexModel> _logger = null;
-            IndexModel indexModel = new IndexModel(_logger);
-            indexModel.OnGet();
+        { 
+            //ILogger<IndexModel> _logger = null;
+            //IndexModel indexModel = new IndexModel(_logger);
+            //indexModel.OnGet();
             Assert.Fail("Test 'Fail Test'");
         }
     }
@@ -31,11 +30,10 @@ namespace NUnitTestProject1
 
         [Test]
         public void Test1()
-        {
-            throw new System.Exception("NUnitTestProject1.MyTests.Test1");
-            ILogger<IndexModel> _logger = null;
-            IndexModel indexModel = new IndexModel(_logger);
-            indexModel.OnGet();
+        { 
+            //ILogger<IndexModel> _logger = null;
+            //IndexModel indexModel = new IndexModel(_logger);
+            //indexModel.OnGet();
             Assert.Fail("MyTests 'Fail Test'");
         }
     }
