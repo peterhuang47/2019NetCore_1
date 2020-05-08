9b8f3c4d80f081b700ec7319fb7c81a7f0ba55b9
Peter <g5447@hotmail.com>
2020-05-08 11:17:11 +0800

    Add New WebApplication1.Pages.Tests class

---
+++ WebApplication1Tests/IndexModelTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.Pages.Tests
+{
+    [TestClass()]
+    public class IndexModelTests
+    {
+        [TestMethod()]
+        public void OnGetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
